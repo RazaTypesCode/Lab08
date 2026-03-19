63dead2afa719a119f650c880307466ed93aeebe
RazaTypesCode <27100383@lums.edu.pk>
2026-03-19 14:19:22 +0500

    Tests for count city fail since there's no count city.

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -10,4 +10,17 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    public void testCountCity() {
+        CustomList list = new CustomList();
+        City karachi = new City("Karachi", "Sindh");
+        City lahore = new City("Lahore", "Punjab");
+        City islamabad = new City("Islamabad", "ICT");
+        list.addCity(karachi);
+        assertTrue(list.countCity() == 1);
+        list.addCity(lahore);
+        assertTrue(list.countCity() == 2);
+        list.addCity(islamabad);
+        assertTrue(list.countCity() == 3);
+    }
 }
